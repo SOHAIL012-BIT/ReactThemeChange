d476892ff23c71ab9e446229a3a783e7ea07b5d6
Muhammad Sohail <sohailabbas.100.10@gmail.com>
2021-10-10 23:43:43 +0500

    Complete

---
+++ src/components/Content.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Content = () => {
+  return (
+  
+    <div>
+      <h1>MUHAMMAD SOHAIL ABBAS</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque id dolore aspernatur consequuntur dignissimos adipisci aut maxime, commodi magni veritatis reiciendis et nulla deleniti placeat voluptatibus iste repellendus voluptate. Voluptatibus?</p>
+      <a href="https://github.com/SOHAIL012-BIT" >
+      <button className="btn-primary">Github</button>
+     </a>
+     
+      
+    </div>
+    
+  )
+  }
+
